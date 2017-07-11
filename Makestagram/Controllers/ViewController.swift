05523ed88b948bcb39c7321fa7d2dcb2d235a06e
zayalachanze <zaya.gooding242@gmail.com>
2017-07-11 10:07:40 -0400

    Set up all the files

---
+++ Makestagram/Controllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Makestagram
+//
+//  Created by Zaya Gooding on 7/10/17.
+//  Copyright © 2017 Zaya Gooding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
